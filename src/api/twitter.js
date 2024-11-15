0a0be527032ccced1f3fda061e9a9f05e9861564
mofan <blockchain.mofan@gmail.com>
2024-11-15 23:55:16 +0800

    初始化

---
+++ src/api/twitter.js
@@ -0,0 +1,11 @@
+// api/twitter.js
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  // const twitterUrl = 'https://x.com/ycxomzhe/status/1857442859684606177?s=46'; // 目标 Twitter 地址
+  //
+  // // 重定向请求到指定的 Twitter 地址
+  // res.redirect(twitterUrl);
+
+  res.write("Hello World!")
+};
