d62a75a633918ce877b08980cda5de140a1511d3
mofan <blockchain.mofan@gmail.com>
2024-11-16 00:07:02 +0800

    初始化

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,6 @@
 module.exports = (req, res) => {
-  res.status(200).json({message: 'Hello from Vercel!'});
+  const twitterUrl = 'https://x.com/ycxomzhe/status/1857442859684606177?s=46'; // 目标 Twitter 地址
+
+  // 重定向请求到指定的 Twitter 地址
+  res.redirect(twitterUrl);
 };
